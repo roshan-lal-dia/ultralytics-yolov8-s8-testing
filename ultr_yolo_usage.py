5d17caeae0557452bc8aa61732ae997776e5c65c
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2024-04-28 21:05:36 +0530

    removed export code

--- ultr_yolo_usage.py
+++ ultr_yolo_usage.py
@@ -9,11 +9,3 @@ model = YOLO('yolov8n.pt')
 # Train the model using the 'coco8.yaml' dataset for 3 epochs
 results = model.train(data='coco8.yaml', epochs=3)
 
-# Evaluate the model's performance on the validation set
-results = model.val()
-
-# Perform object detection on an image using the model
-results = model('https://ultralytics.com/images/bus.jpg')
-
-# Export the model to ONNX format
-success = model.export(format='onnx')
