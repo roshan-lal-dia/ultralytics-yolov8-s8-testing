9a01c52304356271a09b9e660f5195f63b3c82f9
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2024-04-28 22:52:22 +0530

    cropping based on class succesful

--- ultr_testing_trained_model.py
+++ ultr_testing_trained_model.py
@@ -1,8 +1,12 @@
 #Testing the trained model .pt using opencv
 
+import os
 import cv2
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator
+
+# Create a directory to save the cropped images
+os.makedirs('cropped_books', exist_ok=True)
 
 # Load the pretrained model
 model = YOLO('yolov8n.pt')  # replace 'yolov5.pt' with the path to your YOLOv8 model file
@@ -23,11 +27,21 @@ while True:
         annotator = Annotator(frame)
         
         boxes = r.boxes
-        for box in boxes:
+        for i, box in enumerate(boxes):
             
             b = box.xyxy[0]  # get box coordinates in (left, top, right, bottom) format
             c = box.cls
+            class_name = model.names[int(c)]
             annotator.box_label(b, model.names[int(c)])
+            
+			# Check if the class name is 'book'
+            if class_name == 'book':
+            # Crop the detected object from the frame
+                left, top, right, bottom = map(int, b)
+                cropped = frame[top:bottom, left:right]
+            
+                # Save the cropped image to a file
+                cv2.imwrite(f'cropped_books/book_{i}.jpg', cropped)
 
     # Display the frame
     cv2.imshow('Object Detection', frame)
