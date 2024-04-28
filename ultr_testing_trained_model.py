3b7ea5d3dd851d1ec6e69fae3c27ba937cd7a1cf
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2024-04-28 22:30:41 +0530

    mode succesfully run realtime - used ultrlytics annotator

--- ultr_testing_trained_model.py
+++ ultr_testing_trained_model.py
@@ -2,12 +2,13 @@
 
 import cv2
 from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator
 
 # Load the pretrained model
 model = YOLO('yolov8n.pt')  # replace 'yolov5.pt' with the path to your YOLOv8 model file
 
 # Open the webcam
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 while True:
     # Read a frame from the webcam
@@ -17,13 +18,16 @@ while True:
     results = model.predict(frame)
 
     # Draw the detection results on the frame
-    for i in range(len(results)):
-        xywhcn = results[i]
-        #print(xywhcn)
-        print(results[0])
-        x, y, w, h, class_name, confidence = xywhcn
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(frame, class_name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    for r in results:
+        
+        annotator = Annotator(frame)
+        
+        boxes = r.boxes
+        for box in boxes:
+            
+            b = box.xyxy[0]  # get box coordinates in (left, top, right, bottom) format
+            c = box.cls
+            annotator.box_label(b, model.names[int(c)])
 
     # Display the frame
     cv2.imshow('Object Detection', frame)
